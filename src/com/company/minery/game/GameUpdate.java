5200d199826f118c0b9d41dcaffa45e0d228cebe
pauliusj <jurgelevicius.paulius@gmail.com>
2016-08-29 03:46:42 +0300

    endHandling

--- src/com/company/minery/game/GameUpdate.java
+++ src/com/company/minery/game/GameUpdate.java
@@ -382,19 +382,34 @@ public final class GameUpdate {
 		checkCondition(game);
 	}
 	
+	private void stopPlayers(final Game game) {
+		for(int i = 0; i < game.players.size; i += 1) {
+			final Player player = game.players.get(i);
+			player.isRunning = false;
+			player.movementDirection = MovementDirection.Idle;
+			player.velocityX = 0f;
+		}
+	}
+
 	private boolean checkCondition(final Game game) {
 		// Check condition:
 		for(int i = 0; i < game.players.size; i += 1) {
 			if(game.players.get(i).dead) {
 				if(game.localPlayer().dead) {
-					game.messageTimer = 0;
-					game.message = game.assets.defeatLabel;
+					stopPlayers(game);
+					if(game.message == null || game.message == game.assets.fightLabel) {
+						game.messageTimer = 0;
+						game.message = game.assets.defeatLabel;
+					}
 					game.end();
 					return true;
 				}
 				else {
-					game.messageTimer = 0;
-					game.message = game.assets.victoryLabel;
+					stopPlayers(game);
+					if(game.message == null || game.message == game.assets.fightLabel) {
+						game.messageTimer = 0;
+						game.message = game.assets.victoryLabel;
+					}
 					game.end();
 					return true;
 				}
